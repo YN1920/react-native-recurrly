23ae15e7151956c73cdc032a7a5dd63bcf17f947
YN1920 <121037937+YN1920@users.noreply.github.com>
2026-04-09 12:04:44 +0200

    implement fonts

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -6,51 +6,30 @@ export default function App() {
     const router = useRouter();
 
     return (
-        <SafeAreaView className="flex-1 p-5 bg-background">
-            <Text className="text-xl font-bold text-success">
-                Native App!
-            </Text>
+        <SafeAreaView className="flex-1 bg-background p-5">
+            <Text className="text-5xl font-bold text-primary"> Home Page </Text>
 
             <Pressable
                 onPress={() => router.push('/onboarding')}
                 className="mt-4 rounded bg-primary p-4"
             >
-                <Text className="text-white">Go to Onboarding</Text>
+                <Text className="text-white font-sans-bold">Go to Onboarding</Text>
             </Pressable>
 
             <Pressable
                 onPress={() => router.push('/(auth)/sign-in')}
                 className="mt-4 rounded bg-primary p-4"
             >
-                <Text className="text-white">Go to Sign In</Text>
+                <Text className="text-white font-sans-bold">Go to Sign In</Text>
             </Pressable>
 
             <Pressable
                 onPress={() => router.push('/(auth)/sign-up')}
                 className="mt-4 rounded bg-primary p-4"
             >
-                <Text className="text-white">Go to Sign Up</Text>
+                <Text className="text-white font-sans-bold">Go to Sign Up</Text>
             </Pressable>
 
-            <Pressable
-                onPress={() => router.push('/subscriptions/spotify')}
-            >
-                <Text>Spotify Subscriptions</Text>
-            </Pressable>
-
-            <Link
-                href={{
-                    pathname: "/subscriptions/[id]",
-                    params: { id: "claude" },
-                }}
-                asChild
-            >
-                <Pressable>
-                    <Text>
-                        Claude Max Subscriptions
-                    </Text>
-                </Pressable>
-            </Link>
         </SafeAreaView>
     );
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,23 @@
 import { Stack } from "expo-router";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import "@/global.css"; // ✅ KEEP THIS
+import { useFonts } from "expo-font";
+import "@/global.css";
 
 export default function RootLayout() {
-  return (
-      <SafeAreaProvider>
-        <Stack screenOptions={{ headerShown: false }} />
-      </SafeAreaProvider>
-  );
+    const [fontsLoaded] = useFonts({
+        "sans-bold": require("../assets/fonts/PlusJakartaSans-Bold.ttf"),
+        "sans-regular": require("../assets/fonts/PlusJakartaSans-Regular.ttf"),
+        "sans-medium": require("../assets/fonts/PlusJakartaSans-Medium.ttf"),
+        "sans-extrabold": require("../assets/fonts/PlusJakartaSans-ExtraBold.ttf"),
+        "sans-light": require("../assets/fonts/PlusJakartaSans-Light.ttf"),
+        "sans-semibold": require("../assets/fonts/PlusJakartaSans-SemiBold.ttf"),
+    });
+
+    if (!fontsLoaded) return null;
+
+    return (
+        <SafeAreaProvider>
+            <Stack screenOptions={{ headerShown: false }} />
+        </SafeAreaProvider>
+    );
 }
